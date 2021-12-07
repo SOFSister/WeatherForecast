ac996a0b3da86b25372da0bf0dad053fa86fab77
SOFSister <874280179@qq.com>
2021-12-07 11:24:46 +0800

    完成天气页面 2021.12.7 11:24

--- app/src/main/java/com/example/weatherforecast/MainActivity.java
+++ app/src/main/java/com/example/weatherforecast/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.weatherforecast;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.Window;
 
@@ -18,6 +20,8 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -47,5 +51,23 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+        //申请权限
+        requestPower();
     }
+    public void requestPower() {
+    //判断是否已经赋予权限
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            //如果应用之前请求过此权限但用户拒绝了请求，此方法将返回 true。
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION)) {//这里可以写个对话框之类的项向用户解释为什么要申请权限，并在对话框的确认键后续再次申请权限.它在用户选择"不再询问"的情况下返回false
+            } else {
+                //申请权限，字符串数组内是一个或多个要申请的权限，1是申请权限结果的返回参数，在onRequestPermissionsResult可以得知申请结果
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/example/weatherforecast/ui/weather/today/TodayWeatherFragment.java
+++ app/src/main/java/com/example/weatherforecast/ui/weather/today/TodayWeatherFragment.java
@@ -7,6 +7,9 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,7 +17,12 @@ import android.view.ViewGroup;
 
 import com.example.weatherforecast.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TodayWeatherFragment extends Fragment {
+    private List<Tx> txList = new ArrayList<>();//一个全局的链表 详细天气
+    private List<Tx> otherCityList = new ArrayList<>();//一个全局的链表 其他城市信息
 
     private TodayWeatherViewModel mViewModel;
 
@@ -25,7 +33,23 @@ public class TodayWeatherFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.today_weather_fragment, container, false);
+        View view=inflater.inflate(R.layout.today_weather_fragment, container, false);
+        txList=initTxs();//下面的初始化方法
+        RecyclerView recyclerView = view.findViewById(R.id.recycler);//找到RecyclerView控件
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());//布局管理器
+        recyclerView.setLayoutManager(layoutManager);
+        TxAdapter adapter = new TxAdapter(txList);//适配器对象
+        recyclerView.setAdapter(adapter);//设置适配器为上面的对象
+        recyclerView.addItemDecoration(new DividerItemDecoration(getContext(),DividerItemDecoration.VERTICAL));
+        //其他城市
+        otherCityList=initOtherCityTxs();
+        RecyclerView recyclerOtherCityView = view.findViewById(R.id.other_city_recycler);//找到RecyclerView控件
+        LinearLayoutManager layoutOtherCityManager = new LinearLayoutManager(getContext());//布局管理器
+        recyclerOtherCityView.setLayoutManager(layoutOtherCityManager);
+        TxAdapter adapterOtherCity = new TxAdapter(otherCityList);//适配器对象
+        recyclerOtherCityView.setAdapter(adapterOtherCity);//设置适配器为上面的对象
+        recyclerOtherCityView.addItemDecoration(new DividerItemDecoration(getContext(),DividerItemDecoration.VERTICAL));
+        return view;
     }
 
     @Override
@@ -34,5 +58,24 @@ public class TodayWeatherFragment extends Fragment {
         mViewModel = new ViewModelProvider(this).get(TodayWeatherViewModel.class);
         // TODO: Use the ViewModel
     }
-
+    private List<Tx> initTxs(){
+        List<Tx> txList = new ArrayList<>();
+        Tx weather = new Tx("天气        晴", R.drawable.weather);
+        txList.add(weather);//加入到链表
+        Tx temperature = new Tx("温度        15/5°", R.drawable.temperature);
+        txList.add(temperature);//加入到链表
+        Tx calendar = new Tx("日期        2021/12/6", R.drawable.calendar);
+        txList.add(calendar);//加入到链表
+        Tx windDirection = new Tx("风向        西北风", R.drawable.winddirection);
+        txList.add(windDirection);//加入到链表
+        Tx windForce = new Tx("风速        3级", R.drawable.windforce);
+        txList.add(windForce);//加入到链表
+        return txList;
+    }
+    private List<Tx> initOtherCityTxs(){
+        List<Tx> txList = new ArrayList<>();
+        Tx city = new Tx("北京        晴8/-5°", R.drawable.city);
+        txList.add(city);//加入到链表
+        return txList;
+    }
 }
